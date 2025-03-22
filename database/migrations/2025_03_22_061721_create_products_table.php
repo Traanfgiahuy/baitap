ecf4c4db5b920695d68aaadfe86adac8b91c2daf
gia huy <trangiahuy2020kg@gmail.com>
2025-03-22 13:45:08 +0700

    [Database] Tạo migations

---
+++ database/migrations/2025_03_22_061721_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('short_description');
+            $table->string('content');
+            $table->integer('price');
+            $table->integer('sale_price');
+            $table->string('thumbnail');
+            $table->boolean('status')-> comment('0 là ẩn, 1 là hiển thị');
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
